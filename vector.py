b992b0e8cc02f1941ef8a4b75c95716ef7eab76b
Alexander <akcanadanet@gmail.com>
2019-12-22 15:20:39 -0500

    Create vector.py

---
+++ vector.py
@@ -0,0 +1,26 @@
+class Vector:
+
+	def __init__(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.z = z
+
+	def add(self, vector):
+		self.x += vector.x
+		self.y += vector.y
+		self.z += vector.z
+
+	def subtract(self, vector):
+		self.x -= vector.x
+		self.y -= vector.y
+		self.z -= vector.z
+
+	def multiply(self, vector):
+		self.x *= vector.x
+		self.y *= vector.y
+		self.z *= vector.z
+	
+	def divide(self, vector):
+		self.x /= vector.x
+		self.y /= vector.y
+		self.z /= vector.z
