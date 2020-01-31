d3dde6381a34e9b866a68fe8a4bafe9c2fcb4338
Alexander <akcanadanet@gmail.com>
2020-01-31 18:35:25 -0500

    PNG Repackage

--- image.py
+++ image.py
@@ -1,14 +1,14 @@
 from binascii import hexlify, unhexlify, crc32
-from zlib import decompress
+from zlib import compress, decompress
 
 # PNG Magic Number
 signature = ["0x89", "0x50", "0x4e", "0x47", "0x0d", "0x0a", "0x1a", "0x0a"]
 
 # Takes an integer and converts it to a hexadecimal value
 # This value can be converted back to an integer using "int(n, 16)"
-def hexadecimal(number):
+def hexadecimal(number, size = 2):
 	# Format method magic
-	return "{0:#0{1}x}".format(number, 4)
+	return "{0:#0{1}x}".format(number, size + 2)
 
 def hexJoin(*args):
 	joined = "0x"
@@ -21,6 +21,13 @@ def hexString(*args):
 	for arg in args:
 		string += unhexlify(arg[2:])
 	return string
+
+def hexSplit(value, divider = 2):
+	value = value[2:]
+	split = []
+	for i in range(0, len(value), divider):
+		split.append(hexadecimal(int(value[i:i+divider], 16)))
+	return split
 
 class Colour:
 
@@ -255,5 +262,66 @@ class PNG:
 		for scanline in self.pixels:
 			print([pixel.get() for pixel in scanline])
 
+	# Return byte string format ready for saving
+	# TODO: Increase efficiency or find alternative method
+	def repackage(self):
+		data = []
+
+		# Add PNG Signature
+		data.extend(signature)
+
+		# Add IHDR image header chunk
+		prefix = ["0x49", "0x48", "0x44", "0x52"]
+		chunk = [
+			*hexSplit(hexadecimal(len(self.pixels[0]), size = 8)), # Width
+			*hexSplit(hexadecimal(len(self.pixels), size = 8)),    # Height
+			hexadecimal(8), # Bit depth
+			hexadecimal(6), # Color Type (RGBA)
+			hexadecimal(0), # Compression method
+			hexadecimal(0), # Filter method
+			hexadecimal(0)  # Interlace method
+		]
+		size = hexSplit(hexadecimal(len(chunk), size = 8))
+		checksum = hexSplit(hexadecimal(crc32(hexString(*prefix, *chunk)), size = 8))
+		# Add all the contents to the main data array
+		data.extend([*size, *prefix, *chunk, *checksum])
+
+		# Add IDAT image data chunk
+		prefix = ["0x49", "0x44", "0x41", "0x54"]
+
+		# Get all the colour objects and convert to a list of hexadecimals.
+		uncompressed = []
+		for y, row in enumerate(self.pixels):
+			uncompressed.append(["0x00"]) # No filter
+			for x, pixel in enumerate(row):
+				uncompressed[y].extend(pixel.get())
+
+		# Compress the data using zlib
+		compressed = hexlify(compress(hexString(*[j for i in uncompressed for j in i])))
+
+		# Put into standard "['0x00', '0x01', etc..]" format (currently bytes)
+		chunk = [hexadecimal(int(compressed[i:i+2], 16)) for i in range(0, len(compressed), 2)]
+		size = hexSplit(hexadecimal(len(chunk), size = 8))
+		checksum = hexSplit(hexadecimal(crc32(hexString(*prefix, *chunk)), size = 8))
+
+		# Add all the contents to the main data array
+		data.extend([*size, *prefix, *chunk, *checksum])
+
+		# Add IEND image end chunk
+		prefix = ["0x49", "0x45", "0x4e", "0x44"]
+		# IEND chunks don't contain any data
+		size = ["0x00", "0x00", "0x00", "0x00"]
+		# Although this is less clear of what it's doing, since the IEND chunk
+		# is always the same and is not supposed to contain any data, we can
+		# pre-calculate the CRC32 checksum, which is a mathematical operation.
+		# This will, in short, help with efficiency.
+		checksum = ["0xae", "0x42", "0x60", "0x82"]
+		# Add all the contents to the main data array
+		data.extend([*size, *prefix, *checksum])
+
+		return data
+
 image = PNG("test.png")
-image2 = PNG("test2.png")
+repackagedForm = hexString(*image.repackage())
+with open("dump.png", "wb") as png:
+	png.write(repackagedForm)
